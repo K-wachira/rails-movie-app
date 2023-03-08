e0cc05d41d153b894c9754d003bb2d8eb4c11c6e
k-wachira <abkelvinwachira@gmail.com>
2023-03-08 21:49:08 +0300

    Abides to Rails naming conventions

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root to: 'main#index'
+  root to: 'mains#index'
 end
