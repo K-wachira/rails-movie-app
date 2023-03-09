567374db4238c9a2af930a823cc057d17b898e0b
k-wachira <abkelvinwachira@gmail.com>
2023-03-09 08:21:13 +0300

    Adds routes for the whole application

--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,25 @@ Rails.application.routes.draw do
   # Login
   get 'sign_in', to: 'sessions#new'
   post 'sign_in', to: 'sessions#create'
+
+  # Movies
+  
+  # Watched
+  post "add_watched", to: "movies#add_watched_movie"
+  post "remove_watched", to: "movies#remove_watched_movie"
+  get "watched", to: "movies#watched"
+
+  # Favorite
+  get "favorite", to: "movies#favorite"
+  post "add_favorite", to: "movies#add_favorite_movie"
+  post "remove_favorite", to: "movies#remove_favorite_movie"
+
+  # To Watch
+  get "to_watch", to: "movies#watch_list"
+  post "add_to_watch", to: "movies#add_watch_movie"
+  post "remove_to_watch", to: "movies#remove_watch_movie"
+
+  # Search
+  get 'search', to: 'searchs#search'
+  get "show_movie", to: "movies#show" 
 end
