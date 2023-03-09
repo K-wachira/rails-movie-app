19622d2a1c6cd38ee0516a6689f38d8894ed14d7
k-wachira <abkelvinwachira@gmail.com>
2023-03-09 07:54:16 +0300

    Adds search functionality

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -1,4 +1,7 @@
 class MainsController < ApplicationController
   def index
+    if params[:search]
+      redirect_to url_for('controller': 'searchs', 'action': 'search', 'search': params[:search])
+    end
   end
 end
