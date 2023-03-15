ebb3ad17c125f9c6feaa081b3df029a10ae1f5a5
k-wachira <abkelvinwachira@gmail.com>
2023-03-15 20:17:41 +0300

    Api config refactor

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -3,14 +3,8 @@ class SearchsController < ApplicationController
   end
 
   def search
-    @conn = Faraday.new(
-      url: 'https://www.omdbapi.com/',
-      headers: { 'Content-Type' => 'application/json' }
-    )
-    @response = @conn.get("/") do |req|
-        req.params = { :s => params[:search] }
-        req.params["apikey"] = Rails.application.credentials.dig(:omdb, :apikey)
-    end
-    @response = JSON.parse(@response.body)
+    api_instance = OmdbApi.new
+    search_results = api_instance.search({ :s => params[:search] })
+    @response = JSON.parse(search_results.body)
   end
 end
