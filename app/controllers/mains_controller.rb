e8c90f0506ffd9570faadb7927dc803c29056ef4
k-wachira <abkelvinwachira@gmail.com>
2023-04-25 20:15:29 +0300

    Format fix

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -1,7 +1,12 @@
 class MainsController < ApplicationController
   def index
     if params[:search]
-      redirect_to url_for('controller': 'searchs', 'action': 'search', 'search': params[:search])
+      redirect_to url_for(
+                    controller: "searchs",
+                    action: "search",
+                    search: params[:search],
+                    page: 0 
+                  )
     end
   end
 end
