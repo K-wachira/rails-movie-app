006dc1dc8d24ab37bd91c91244e0709f6d742781
k-wachira <abkelvinwachira@gmail.com>
2023-04-25 20:16:25 +0300

    Introduce comments, and also pagination of search results

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,39 +9,45 @@ class MoviesController < ApplicationController
   def show
     @movie_id = Movie.find_by(id: params["format"])
     api_instance = OmdbApi.new
-    @movie_response = JSON.parse(api_instance.search({ 'i':  @movie_id[:imdbID] }).body)
-    @response2 = JSON.parse(api_instance.search({ 's': @movie_id[:title].split(":", -1)[0] }).body)
+    @movie_response =
+      JSON.parse(api_instance.search({ i: @movie_id[:imdbID] }).body)
+    @response2 =
+      JSON.parse(
+        api_instance.search({ s: @movie_id[:title].split(":", -1)[0] }).body
+      )
+    @movie_comments = Comment.where(movie_id: params["format"])
   end
 
   def add_watched_movie
-     if find_by_imdbID(movie_params[:imdbID]) == nil
-       watched_movie = Movie.new(movie_params.merge!(watched: true))
-       if watched_movie.save
-           redirect_to watched_path, notice: "Successfully added movie to watched"
-       else
-           redirect_to root_path, alert: " Not okay"
-       end
-     else
-       watched_movie= Movie.find_by(imdbID: movie_params[:imdbID])
-       if watched_movie.update(watched: true)
-         redirect_to watched_path, notice: "Successfully added movie to watched"
-       else
-         redirect_to root_path, alert: " Not okay"
-       end
-     end
+    if find_by_imdbID(movie_params[:imdbID]) == nil
+      watched_movie = Movie.new(movie_params.merge!(watched: true))
+      if watched_movie.save
+        redirect_to watched_path, notice: "Successfully added movie to watched"
+      else
+        redirect_to root_path, alert: " Not okay"
+      end
+    else
+      watched_movie = Movie.find_by(imdbID: movie_params[:imdbID])
+      if watched_movie.update(watched: true)
+        redirect_to watched_path, notice: "Successfully added movie to watched"
+      else
+        redirect_to root_path, alert: " Not okay"
+      end
+    end
   end
 
   def remove_watched_movie
-     if find_by_imdbID(movie_params[:imdbID]) == nil
-        redirect_to root_path, alert: " Not okay"
-     else
-       watched_movie= Movie.find_by(imdbID: movie_params[:imdbID])
-       if watched_movie.update(watched: false)
-         redirect_to watched_path, notice: "Successfully removed movie to watched"
-       else
-         redirect_to watched_path, alert: " Not okay"
-       end
-     end
+    if find_by_imdbID(movie_params[:imdbID]) == nil
+      redirect_to root_path, alert: " Not okay"
+    else
+      watched_movie = Movie.find_by(imdbID: movie_params[:imdbID])
+      if watched_movie.update(watched: false)
+        redirect_to watched_path,
+                    notice: "Successfully removed movie to watched"
+      else
+        redirect_to watched_path, alert: " Not okay"
+      end
+    end
   end
 
   def watched
@@ -49,7 +55,8 @@ class MoviesController < ApplicationController
     if session[:user_id]
       @watched_movies = Movie.where(user_id: Current.user.id, watched: true)
     else
-      redirect_to sign_in_path, notice: "You have to be logged in to view the page"
+      redirect_to sign_in_path,
+                  notice: "You have to be logged in to view the page"
     end
   end
 
@@ -59,71 +66,76 @@ class MoviesController < ApplicationController
     if session[:user_id]
       @favorite_movies = Movie.where(user_id: Current.user.id, liked: true)
     else
-      redirect_to sign_in_path, notice: "You have to be logged in to view the page"
+      redirect_to sign_in_path,
+                  notice: "You have to be logged in to view the page"
     end
   end
 
   def add_favorite_movie
-     if find_by_imdbID(movie_params[:imdbID]) == nil
-         favorite_movie = Movie.new(movie_params.merge!(liked: true, to_watch: false))
-         if favorite_movie.save
-             redirect_to favorite_path, notice: "Successfully liked movie"
-         else
-             redirect_to root_path, alert: " Not okay"
-         end
-     else
-       favorite_movie = Movie.find_by(imdbID: movie_params[:imdbID])
-       if favorite_movie.update(liked: true, to_watch: false)
-         redirect_to favorite_path, notice: "Successfully liked movie"
-       else
-         redirect_to root_path, alert: " Not okay"
-       end
-     end
-  end
-  
-  def remove_favorite_movie
-     if find_by_imdbID(movie_params[:imdbID]) == nil
+    if find_by_imdbID(movie_params[:imdbID]) == nil
+      favorite_movie =
+        Movie.new(movie_params.merge!(liked: true, to_watch: false))
+      if favorite_movie.save
+        redirect_to favorite_path, notice: "Successfully liked movie"
+      else
         redirect_to root_path, alert: " Not okay"
-     else
-       favorite_movie = Movie.find_by(imdbID: movie_params[:imdbID])
-       if favorite_movie.update(liked: false)
-         redirect_to favorite_path, notice: "Successfully unliked movie"
-       else
-         redirect_to root_path, alert: " Not okay"
-       end
-     end
+      end
+    else
+      favorite_movie = Movie.find_by(imdbID: movie_params[:imdbID])
+      if favorite_movie.update(liked: true, to_watch: false)
+        redirect_to favorite_path, notice: "Successfully liked movie"
+      else
+        redirect_to root_path, alert: " Not okay"
+      end
+    end
+  end
+
+  def remove_favorite_movie
+    if find_by_imdbID(movie_params[:imdbID]) == nil
+      redirect_to root_path, alert: " Not okay"
+    else
+      favorite_movie = Movie.find_by(imdbID: movie_params[:imdbID])
+      if favorite_movie.update(liked: false)
+        redirect_to favorite_path, notice: "Successfully unliked movie"
+      else
+        redirect_to root_path, alert: " Not okay"
+      end
+    end
   end
 
   # Watch list movie Methods -------------------------------
   def add_watch_movie
-     if find_by_imdbID(movie_params[:imdbID]) == nil
-       watch_list_movie = Movie.new(movie_params.merge!(to_watch: true))
-       if watch_list_movie.save
-           redirect_to to_watch_path, notice: "Successfully added movie to watch list"
-       else
-           redirect_to root_path, alert: " Not okay"
-       end
-     else
-       watch_list_movie = Movie.find_by(imdbID: movie_params[:imdbID])
-       if watch_list_movie.update(to_watch: true)
-         redirect_to to_watch_path, notice: "Successfully added movie to watch list"
-       else
-         redirect_to root_path, alert: " Not okay"
-       end
-     end
+    if find_by_imdbID(movie_params[:imdbID]) == nil
+      watch_list_movie = Movie.new(movie_params.merge!(to_watch: true))
+      if watch_list_movie.save
+        redirect_to to_watch_path,
+                    notice: "Successfully added movie to watch list"
+      else
+        redirect_to root_path, alert: " Not okay"
+      end
+    else
+      watch_list_movie = Movie.find_by(imdbID: movie_params[:imdbID])
+      if watch_list_movie.update(to_watch: true)
+        redirect_to to_watch_path,
+                    notice: "Successfully added movie to watch list"
+      else
+        redirect_to root_path, alert: " Not okay"
+      end
+    end
   end
 
   def remove_watch_movie
-     if find_by_imdbID(movie_params[:imdbID]) == nil
+    if find_by_imdbID(movie_params[:imdbID]) == nil
+      redirect_to root_path, alert: " Not okay"
+    else
+      watch_list_movie = Movie.find_by(imdbID: movie_params[:imdbID])
+      if watch_list_movie.update(to_watch: false)
+        redirect_to to_watch_path,
+                    notice: "Successfully removed movie to watch list"
+      else
         redirect_to root_path, alert: " Not okay"
-     else
-       watch_list_movie = Movie.find_by(imdbID: movie_params[:imdbID])
-       if watch_list_movie.update(to_watch: false)
-         redirect_to to_watch_path, notice: "Successfully removed movie to watch list"
-       else
-         redirect_to root_path, alert: " Not okay"
-       end
-     end
+      end
+    end
   end
 
   def watch_list
@@ -131,15 +143,22 @@ class MoviesController < ApplicationController
     if session[:user_id]
       @watch_list_movies = Movie.where(user_id: Current.user.id, to_watch: true)
     else
-      redirect_to sign_in_path, notice: 'You have to be logged in to view the page'
+      redirect_to sign_in_path,
+                  notice: "You have to be logged in to view the page"
     end
   end
 
   private
 
   def search
+    puts "It does hit the movie controller"
     if params[:search]
-       redirect_to url_for(:controller => :searchs, :action => :search, :search => params[:search])
+      redirect_to url_for(
+                    controller: :searchs,
+                    action: :search,
+                    search: params[:search],
+                    page: 0
+                  )
     end
   end
 
@@ -147,16 +166,20 @@ class MoviesController < ApplicationController
     return Movie.find_by(imdbID: imdbID)
   end
 
+  def find_by_user_byID(user_id)
+    return User.find_by(user_id: user_id)
+  end
+
   def movie_params
-    @params = params.as_json['movie'].as_json.keys[0].dup
-    if @params.include? '=>'
-      @params.gsub! '=>', ':'
-    end
+    @params = params.as_json["movie"].as_json.keys[0].dup
+    @params.gsub! "=>", ":" if @params.include? "=>"
     obj = ActiveSupport::JSON.decode(@params)
-    { imdbID: obj['imdbID'],
+    {
+      imdbID: obj["imdbID"],
       user_id: Current.user.id,
-      poster: obj['Poster'] || obj['poster'],
-      title: obj['Title'] || obj['title'],
-      category: obj['Type'] || obj['type'] }
+      poster: obj["Poster"] || obj["poster"],
+      title: obj["Title"] || obj["title"],
+      category: obj["Type"] || obj["type"]
+    }
   end
 end
--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -1,9 +1,13 @@
-class SearchsController < ApplicationController 
+class SearchsController < ApplicationController
   def new
   end
 
   def search
-    search_results = OmdbApi.new.search({ :s => params[:search] })
+    current_page = params[:page].to_i 
+    search_results =
+      OmdbApi.new.search(
+        { s: params[:search], y: params[:year], type: params[:type], p: current_page + 1 }
+      )
     @response = JSON.parse(search_results.body)
   end
 end
